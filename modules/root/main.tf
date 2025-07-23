6ca05cc3b7a87bf2218f8e3f98276732ca01b218
VStefanov <v.s.stefanov@gmail.com>
2025-07-23 18:54:58 +0300

    adjust renovate config

--- modules/root/main.tf
+++ modules/root/main.tf
@@ -1,7 +1,7 @@
 module "ec2" {
-  source = "git::https://github.com/VStefanov/tf.git//modules/ec2?ref=v0.0.1"
+  source = "git::https://github.com/VStefanov/tf.git//modules/ec2?ref=2d9758d"
 }
 
 module "rds" {
-  source = "git::https://github.com/VStefanov/tf.git//modules/rds?ref=v0.0.1"
+  source = "git::https://github.com/VStefanov/tf.git//modules/rds?ref=2d9758d"
 }
