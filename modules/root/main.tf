5c4c607fa9959dae698f31f4563e7984446579b5
VStefanov <v.s.stefanov@gmail.com>
2025-07-24 23:43:17 +0300

    test

--- modules/root/main.tf
+++ modules/root/main.tf
@@ -1,7 +1,7 @@
 module "ec2" {
-  source = "git::https://github.com/VStefanov/tf.git//modules/ec2?ref=ec2-v0.0.1"
+  source = "git::https://github.com/VStefanov/tf.git//modules/ec2?ref=v0.0.1"
 }
 
 module "rds" {
-  source = "git::https://github.com/VStefanov/tf.git//modules/rds?ref=rds-v0.0.1"
+  source = "git::https://github.com/VStefanov/tf.git//modules/rds?ref=v0.0.1"
 }
