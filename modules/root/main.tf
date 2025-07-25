a0e55d5f871bae7853b0bb70ab71c29a1386e0ff
VStefanov <v.s.stefanov@gmail.com>
2025-07-25 15:18:15 +0300

    fix

--- modules/root/main.tf
+++ modules/root/main.tf
@@ -6,6 +6,6 @@ module "rds" {
   source = "git::https://github.com/VStefanov/tf.git//modules/rds?ref=rds-v0.0.1"
 }
 
-module "rds" {
+module "s3" {
   source = "git::https://github.com/VStefanov/tf.git//modules/s3?ref=s3-v0.0.1"
 }
